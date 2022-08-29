4d9438c87f178bfc78f25d747d18de19b4881a74
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-29 02:09:59 -0300

    feat: Cria componente de Item

---
+++ src/components/Item.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Text } from "react-native";
+
+interface Props {
+  data: {
+    name: string;
+    likes: number;
+  };
+}
+
+export function Item({ data }: Props) {
+  return (
+    <Text>
+      {data.name} - likes: {data.likes}
+    </Text>
+  );
+}
