3375b8047b95e449fa0209513f8510f51aaebcca
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-09-02 02:26:22 -0300

    feat: Instala biblioteca lodash

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 
 import { Text, View, TouchableOpacity } from "react-native";
 
+import lodash from "lodash";
 interface Props {
   data: {
     id: number;
@@ -31,5 +32,5 @@ function FriendComponent({ data, follow }: Props) {
 }
 
 export const Friend = memo(FriendComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps.data, nextProps.data);
+  return lodash.isEqual(prevProps.data, nextProps.data);
 });
